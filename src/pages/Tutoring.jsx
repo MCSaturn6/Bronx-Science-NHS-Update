c23fbf0dc517042b60289df964992d3cf639d8dc
Matthew Greenspun <78003195+MatthewGreenspun@users.noreply.github.com>
2023-09-13 18:55:06 -0400

    update tutoring form

--- src/pages/Tutoring.jsx
+++ src/pages/Tutoring.jsx
@@ -1,25 +1,39 @@
 import React, { Component } from "react";
-import { useEffect } from 'react'
+import { useEffect } from "react";
 import Hero from "../components/Hero.jsx";
 
 export default function Tutoring() {
   useEffect(() => {
-    window.scrollTo(0, 0)
-  }, [])
+    window.scrollTo(0, 0);
+  }, []);
   return (
     <section className="page pb-16">
       <Hero
-      title="Tutoring"
-      image="https://www.bxscience.edu/pics/mainLibraryeading19.jpg"
+        title="Tutoring"
+        image="https://www.bxscience.edu/pics/mainLibraryeading19.jpg"
       />
       <section className="body-text pt-6">
         <div class="page-body-text">
-
-        <h2 className="text-3xl font-bold mt-6 mb-3 underline decoration-yellow-500 underline-offset-8">
-          Documenting Tutoring Hours (for NHS Members)
+          <h2 className="text-3xl font-bold mt-6 mb-3 underline decoration-yellow-500 underline-offset-8">
+            Documenting Tutoring Hours (for NHS Members)
           </h2>
           <p>
-          NHS members who are matched with a tutor must document their tutoring hours using <a href="https://www.bxscience.edu/ourpages/auto/2014/4/24/65108071/NHS%20Tutoring%20Form.pdf" target="_blank">this</a> log. Uplaod the log at the end of each semester <a href="https://docs.google.com/forms/d/e/1FAIpQLSeTSMYDdIc9Eusj27kwEXhmZoZb9fosGQz0Jdtm23JfpTV3RA/viewform" target="_blank">here</a>.
+            NHS members who are matched with a tutor must document their
+            tutoring hours using{" "}
+            <a
+              href="https://www.bxscience.edu/ourpages/auto/2014/4/24/65108071/NHS%20Tutoring%20Form.pdf"
+              target="_blank"
+            >
+              this
+            </a>{" "}
+            log. Uplaod the log at the end of each semester{" "}
+            <a
+              href="https://docs.google.com/forms/d/e/1FAIpQLSeTSMYDdIc9Eusj27kwEXhmZoZb9fosGQz0Jdtm23JfpTV3RA/viewform"
+              target="_blank"
+            >
+              here
+            </a>
+            .
           </p>
           <br></br>
           <br></br>
@@ -28,7 +42,14 @@ export default function Tutoring() {
             Requesting a Tutor (for Students)
           </h2>
           <p>
-          If you would like assistance in any of your subjects, please use <a href="https://docs.google.com/forms/d/e/1FAIpQLSfvi6WwtPg67LyQNDJ1Gikk6dggVUF_oibhug6B3xbMuO7nCg/viewform" target="_blank">this</a> form to request a tutor. 
+            If you would like assistance in any of your subjects, please use{" "}
+            <a
+              href="https://docs.google.com/document/d/13rymvhpj5mqVk9mhyyL_SIg1nc6ucN6ZYK8CKDrOXeY/edit?usp=drive_link"
+              target="_blank"
+            >
+              this
+            </a>{" "}
+            form to request a tutor.
           </p>
           <br></br>
           <br></br>
@@ -37,7 +58,15 @@ export default function Tutoring() {
             Requesing a Tutor (for Teachers)
           </h2>
           <p>
-          Teachers may request an NHS tutor to assist them during SGI by submitting <a href="https://docs.google.com/forms/d/e/1FAIpQLScwbBzHgWnDTu6L9YpjqVxYo4UCBo5SdGpQzwWdJgN8gf8JYA/viewform" target="_blank">this</a> form.
+            Teachers may request an NHS tutor to assist them during SGI by
+            submitting{" "}
+            <a
+              href="https://docs.google.com/document/d/1-uCUkzfpnp7kv0LXBwj6mqAvKTW8Nmc2tcKPKpbJEiY/edit"
+              target="_blank"
+            >
+              this
+            </a>{" "}
+            form.
           </p>
           <br></br>
           <br></br>
@@ -46,7 +75,15 @@ export default function Tutoring() {
             Qualifying a Tutor (for Teachers)
           </h2>
           <p>
-          Please use <a href="https://docs.google.com/forms/d/e/1FAIpQLSdgAFUTGbrrBstj6KcHTk0ZEqasbEzpCcYtzwGU9H5xRLzQsg/viewform" target="_blank">this</a> google form to verify that your non-NHS member student is qualified to tutor in the subject that you teach.
+            Please use{" "}
+            <a
+              href="https://docs.google.com/forms/d/e/1FAIpQLSdgAFUTGbrrBstj6KcHTk0ZEqasbEzpCcYtzwGU9H5xRLzQsg/viewform"
+              target="_blank"
+            >
+              this
+            </a>{" "}
+            google form to verify that your non-NHS member student is qualified
+            to tutor in the subject that you teach.
           </p>
           <br></br>
           <br></br>
