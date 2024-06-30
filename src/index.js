88592cf5b1f09a9a71c5aeb5bf6394a686b2baaa
Riccardo Sambataro <ricky.sambataro@gmail.com>
2024-06-30 14:15:16 +0200

    analystics

--- src/index.js
+++ src/index.js
@@ -4,8 +4,11 @@ import { BrowserRouter } from "react-router-dom";
 import { SpeedInsights } from "@vercel/speed-insights/react";
 import App from "./components/App.jsx";
 import 'bootstrap/dist/css/bootstrap.css';
+import { Analytics } from "@vercel/analytics/react"
+
 
 ReactDOM.render(
    <BrowserRouter><App/></BrowserRouter>, document.getElementById("root"));
    <SpeedInsights />
+   <Analytics/>
 
