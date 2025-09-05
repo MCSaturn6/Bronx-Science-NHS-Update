95ff133809886cf9f283940a959aa62db16c5626
MCSaturn6 <MCSaturn6>
2025-09-05 00:37:34 -0400

    update names

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -19,7 +19,7 @@ const board = [
     email: "kyleq3@nycstudents.net",
   },
   {
-    name: "Menqian Cai",
+    name: "Menqian",
     position: "VP of Technology",
     email: "Menqianc@nycstudents.net",
   },
